011473a0c256eea883ba67c3bacd55d5c3aa4a92
og <guanxiqinms@163.com>
2017-08-31 08:30:37 +0800

    地点

---
+++ Ogau/src/com/ogau/socialshare/SocialShareTestActivity.java
@@ -0,0 +1,45 @@
+package com.ogau.socialshare;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.ogau.R;
+import com.ogau.base.BaseActivity;
+
+public class SocialShareTestActivity extends BaseActivity implements OnClickListener {
+
+	private Button bt_share_mob;
+
+	@Override
+	protected void onCreate(@Nullable Bundle arg0) {
+		// TODO Auto-generated method stub
+		super.onCreate(arg0);
+		setContentView(R.layout.activity_social_share_test);
+
+		initViews();
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.bt_share_mob:
+			gotoMobShareActivity();
+			break;
+		}
+
+	}
+	private void gotoMobShareActivity(){
+		
+	}
+
+	private void initViews() {
+		bt_share_mob = (Button) findViewById(R.id.bt_share_mob);
+
+		bt_share_mob.setOnClickListener(this);
+	}
+
+}
---
+++ Ogau/src/com/ogau/socialshare/mobshare/MobShareActvity.java
@@ -0,0 +1,16 @@
+package com.ogau.socialshare.mobshare;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.ogau.base.BaseActivity;
+
+public class MobShareActvity extends BaseActivity implements OnClickListener {
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
